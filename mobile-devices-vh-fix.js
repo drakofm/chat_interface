87022daea8c0edaf63c5d218ed2abafdb7d62e4b
Igor Grabeklis <drakofm@gmail.com>
2024-10-02 10:53:54 +0300

    Trying to solve 100vh on mobile devices problem [7]

--- mobile-devices-vh-fix.js
+++ mobile-devices-vh-fix.js
@@ -1,8 +1,7 @@
 const setViewportProperty = () => {
   let vh = window.innerHeight * 0.01;
-  document.querySelector(main).style.setProperty('--vh', `${vh}px`);
-}
-
+  document.querySelector('main').style.setProperty('--vh', `${vh}px`);
+} 
 window.addEventListener('resize', setViewportProperty);
 window.addEventListener('orientationchange', setViewportProperty);
 setViewportProperty();
